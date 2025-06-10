4b9494c6a2be27dd891f827d10e14b0169f32ffa
Pedro Vieira <phbattistellavieira@gmail.com>
2025-06-10 18:23:03 -0300

    Fixed identation for black

--- leetcode/2/2.py
+++ leetcode/2/2.py
@@ -3,16 +3,18 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+
 class Solution:
-    def addTwoNumbers(self, l1: ListNode | None, l2: ListNode | None) -> ListNode | None:
+    def addTwoNumbers(
+        self, l1: ListNode | None, l2: ListNode | None
+    ) -> ListNode | None:
         a = []
         b = []
-        while(l1.val != 0 and l1.next != None):
+        while l1.val != 0 and l1.next != None:
             print("here")
             a.add(l1.val)
-            
-            
-            
+
         print("a")
         print(a)
         print("end")
@@ -24,4 +26,3 @@ class Solution:
         # print(c)
         # abc = (c * 100) + (b * 10) + a
         # print(abc)
-
